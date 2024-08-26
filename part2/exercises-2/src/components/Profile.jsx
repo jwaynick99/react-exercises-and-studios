004a6e1c882fb5d39e366e63addda529952c3470
jwaynick99 <jwaynick99@gmail.com>
2024-08-25 21:29:46 -0500

    React 2 Exercises

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,15 +1,24 @@
-import './styles.css';
-import Button from './Button';
+import "./styles.css";
+import Button from "./Button";
+import oceans from "./oceans.json";
 
-
-function Profile()
-{
-   return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
-      </>  
-   );
+function Profile() {
+    let listitem = oceans.map((ocean) => (
+        <div key={ocean.id} className="profile">
+            <img src={ocean.image} alt={ocean.name} className={ocean.fishCheck === "true" ? "isAFish" : "profile"} />
+            <h1>{ocean.name}</h1>
+            <h3>
+                Fun Facts:
+                <ol>
+                    <li>{ocean.fact1}</li>
+                    <li>{ocean.fact2}</li>
+                    <li>{ocean.fact3}</li>
+                </ol>
+            </h3>
+            <Button />
+        </div>
+    ));
+    return <ul>{listitem}</ul>;
 }
 
 export default Profile;
